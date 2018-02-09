6d1002cb7e2c3f2cb13b99de349b197699519ba8
Kayra <KayraAlat@gmail.com>
2018-02-09 20:19:00 +0000

    Added setup

--- tornado-app/setup.py
+++ tornado-app/setup.py
@@ -1,1 +1,23 @@
 from setuptools import setup, find_packages
+
+requires = [
+    'tornado',
+    'tornado-sqlalchemy',
+    'psycopg2'
+]
+
+setup(
+    name='tornado-app',
+    version='0.0',
+    description='A To-Do List built with Tornado',
+    author='Kayra',
+    author_email='Kayra@test.com',
+    keywords='web tornado',
+    packages=find_packages(),
+    install_requires=requires,
+    entry_points={
+        'console_scripts': [
+            'serve_app = todo:main'
+        ]
+    }
+)
