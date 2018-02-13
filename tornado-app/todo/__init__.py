eedcf4b9e6e52f1f4c76fd6b584ec726a19fca44
Kayra <KayraAlat@gmail.com>
2018-02-13 20:45:00 +0000

    Updated init to full server

--- tornado-app/todo/__init__.py
+++ tornado-app/todo/__init__.py
@@ -1,8 +1,21 @@
+from tornado.options import define, options
+from tornado.httpserver import HTTPServer
 from tornado.web import Application
+from tornado.ioloop import IOLoop
+
+
+define('port', default=8888, help='This is the port to listen on.')
 
 
 def main():
     """
     Construct and serve the tornado application.
     """
+
     app = Application()
+    http_server = HTTPServer(app)
+    http_server.listen(options.port)
+
+    print(f"Listening on http:127.0.0.1:{options.port}")
+
+    IOLoop.current().start()
